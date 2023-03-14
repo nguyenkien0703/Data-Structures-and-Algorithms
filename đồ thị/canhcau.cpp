890c4790e7be93ec39ef8de0080948e9306ef13c
Nguyễn Văn KIên <94183591+nguyenkien0703@users.noreply.github.com>
2023-03-15 00:40:52 +0700

    Update canhcau.cpp

--- đồ thị/canhcau.cpp
+++ đồ thị/canhcau.cpp
@@ -1,71 +1,76 @@
 #include <bits/stdc++.h>
+#define endl "\n"
 using namespace std;
-
-#define ll long long
-#define pii pair<ll, ll> 
-#define vii vector<pii> 
-#define pb push_back 
-#define fi first 
-#define el cout<<endl
-#define se second 
-#define in(a) cin>>  a;
-#define out(a) cout<<  a;
-#define all(x) (x).begin(), (x).end()
-#define rep(a) for(int i = 0; i < (a); i++)
-#define vll vector<ll> 
-#define vi vector<int >
-#define ms(a,n) memset(a,n,sizeof(a))
-#define FOR(i,a,b) for(int i=a;i<=b;i++)
-#define FOD(i,b,a) for(int i=b;i>=a;i--)
-#define faster ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
-const ll MOD = 1e9 + 7;
-
-int n, m;
-vll ke[10001];
-int low[1001], disc[10001], timer = 0;
-vii cau;
-
-void  dfs(int u ,int par){
-    low[u] = disc[u] = ++timer;
-    for (int v :  ke[u]){
-        
-        if(v==par)
-            continue;
-        if(disc[v]==0){
-            dfs(v, u);
-            if(low[v]>disc[u]){
-                cau.pb({u,v});
+vector<bool> check;
+vector<vector<int>> a;
+void bfs(int n, int x, int y)
+{
+    queue<int> q;
+    q.push(n);
+    while (q.size())
+    {
+        int u = q.front();
+        q.pop();
+        for (auto v : a[u])
+        {
+            if ((u == x && v == y) || (u == y && v == x))
+                continue;
+            if (check[v])
+            {
+                check[v] = 0;
+                q.push(v);
             }
-            low[u]=min(low[u],low[v]);
-        }else {
-            low[u] = min(low[u], disc[v]);
         }
     }
 }
-void solve(){
-    cin >> n >> m;
-    for (int i = 0; i < m;i++){
-        int x, y;cin>>x >> y;
-        ke[x].pb(y);
-        ke[y].pb(x);
-    }
-    for (int i = 1; i <= n;i++){
-        sort(all(ke[i]));
-    }
-    for (int i = 1; i <= n;i++){
-        if(disc[i]==0){
-
-            dfs(i, -1);
-        }
-
-    }
-    cout << cau.size();
-    
-}
-
 int main()
 {
-    
-    faster;
-    solve();
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, m, stp = 0, s;
+        cin >> n >> m;
+        a.clear();
+        a.resize(n + 1);
+        check.clear();
+        check.resize(n + 1, 1);
+        vector<pair<int, int>> e(m);
+        for (int i = 0; i < m; i++)
+        {
+            cin >> e[i].first >> e[i].second;
+            a[e[i].first].push_back(e[i].second);
+            a[e[i].second].push_back(e[i].first);
+        }
+        sort(e.begin(), e.end());
+        for (int i = 1; i <= n; i++)
+        {
+            if (check[i])
+            {
+                stp++;
+                check[i] = 0;
+                bfs(i, -1, -1);
+            }
+        }
+        for (int i = 0; i < m; i++)
+        {
+            check.clear();
+            check.resize(n + 1, 1);
+            s = 0;
+            for (int j = 1; j <= n; j++)
+            {
+                if (check[j])
+                {
+                    s++;
+                    check[j] = 0;
+                    bfs(j, e[i].first, e[i].second);
+                }
+            }
+            if (s > stp)
+                cout << e[i].first << " " << e[i].second << " ";
+        }
+        cout << endl;
+    }
 }
