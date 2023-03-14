e2d31eac088c181422172d0ada512fb876fd5b8c
Nguyễn Văn KIên <94183591+nguyenkien0703@users.noreply.github.com>
2023-03-15 00:51:36 +0700

    Update demsothanhphanlienthong.cpp

--- đồ thị/demsothanhphanlienthong.cpp
+++ đồ thị/demsothanhphanlienthong.cpp
@@ -1,87 +1,47 @@
 #include <bits/stdc++.h>
+#define endl "\n"
 using namespace std;
-
-#define ll long long
-#define pii pair<ll, ll> 
-#define vii vector<pii> 
-#define pb push_back 
-#define fi first 
-#define el cout<<endl
-#define se second 
-#define in(a) cin>>  a;
-#define out(a) cout<<  a;
-#define all(x) (x).begin(), (x).end()
-#define rep(a) for(int i = 0; i < (a); i++)
-#define vll vector<ll> 
-#define vi vector<int >
-#define ms(a,n) memset(a,n,sizeof(a))
-#define FOR(i,a,b) for(int i=a;i<=b;i++)
-#define FOD(i,b,a) for(int i=b;i>=a;i--)
-#define faster ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
-const ll MOD = 1e9 + 7;
-
-
-
-vll ke[1001];
-int used[1001];
-
-int n , m , u;
-void dfs(int u){
-    used[u]=1;
-    cout<<u<<' ';
-    for(int x : ke[u]){
-        if(!used[x]){
-            dfs(x);
-        }
-    }
-}
-void bfs(int u){
-    
-   queue<int > q;
-   q.push(u);
-
-   used[u]=1;
-   while(!q.empty()){
-        int top=q.front();
-        q.pop();
-        //cout<< top<<' ';
-        for(int x : ke[top]){
-            if(!used[x]){
-                q.push(x);
-                used[x]=1;
-            }
-        }
-   }
-}
-void solve(){
-   cin>>n>>m;
-   for(int i=0;i<m;i++){
-        int x, y;
-        cin>>x>>y;
-        ke[x].pb(y);
-        ke[y].pb(x);
-   }
-   ms(used,0);
-   int cnt =0;
-   for(int i=1;i<=n;i++){
-    if(!used[i]){
-        ++cnt ;
-        bfs(i);
-    }
-   }
-   cout<<cnt <<endl;
-
-}
-
-
-
 int main()
 {
-    #ifndef ONLINE_JUDGE
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
-    #endif 
-    faster;
-    solve();    
-
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, m, x, y, s = 0, k;
+        cin >> n >> m;
+        int check[n + 1] = {};
+        vector<vector<int>> a(n + 1);
+        for (int i = 0; i < m; i++)
+        {
+            cin >> x >> y;
+            a[x].push_back(y);
+            a[y].push_back(x);
+        }
+        for (int i = 1; i <= n; i++)
+        {
+            if (!check[i])
+            {
+                s++;
+                check[i] = 1;
+                stack<int> st;
+                st.push(i);
+                while (st.size())
+                {
+                    k = st.top();
+                    st.pop();
+                    for (auto j : a[k])
+                    {
+                        if (!check[j])
+                        {
+                            check[j] = 1;
+                            st.push(j);
+                        }
+                    }
+                }
+            }
+        }
+        cout << s << endl;
+    }
 }
